1240261de53bf7af57aaef98e22771590e2f9a4d
Anna Gavrilova <agavrilova@Annas-MacBook-Pro.local>
2020-08-24 23:46:08 -0400

    horoshee soobshenie

---
+++ i Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i Am Rich
+//
+//  Created by Anna Gavrilova on 2020-08-13.
+//  Copyright © 2020 Anna Gavrilova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
